4c0f9c46130ba482d1bb5afdcd3aea74c2949af1
hdbg <httpdebugger@protonmail.com>
2026-01-09 21:29:13 +0100

    fix(group_retriever): filter outdated groups

--- lib/screens/groups_retriever.dart
+++ lib/screens/groups_retriever.dart
@@ -11,6 +11,54 @@ final _jsPayload = '''
 const student_group_table=document.querySelector(".rlbGroup.rlbGroupRight");var groups=[];for(const a of student_group_table.querySelectorAll("span.rlbText")){const b=a.innerText;groups.push(b)};groups
 ''';
 
+/* 
+PJATK's website returns all student groups, including past ones.
+This function:
+- Find highest semester groups (second element if spaced by space, prefixed with 'I.')
+- Leaves only groups with last semester
+
+Special exclusion is groups, where last element is "(ITN)".
+Those are repetition groups, which are relevant.
+They should go through the same filtering as regular groups, but separately
+*/
+Set<String> _filterGroups(Set<String> groups) {
+  const semesterPrefix = 'I.';
+  const repetitionSuffix = '(ITN)';
+
+  final splitted = groups.map((e) => e.split(' ')).toList();
+  var highestSemester = null as int?;
+  var highestRepSemester = null as int?;
+
+  for (final parts in splitted) {
+    final semesterPart = parts[1].replaceAll(semesterPrefix, '');
+    final semester = int.tryParse(semesterPart);
+    if (semester == null) continue;
+
+    if (parts.last == repetitionSuffix) {
+      if (highestRepSemester == null || semester > highestRepSemester) {
+        highestRepSemester = semester;
+      }
+    } else {
+      if (highestSemester == null || semester > highestSemester) {
+        highestSemester = semester;
+      }
+    }
+  }
+
+  return splitted
+      .where((group) {
+        final semesterPart = group[1].replaceAll(semesterPrefix, '');
+        final semester = int.tryParse(semesterPart);
+        if (group.last == repetitionSuffix) {
+          return semester == highestRepSemester;
+        } else {
+          return semester == highestSemester;
+        }
+      })
+      .map((e) => e.join(' '))
+      .toSet();
+}
+
 class GroupsRetriever extends ConsumerWidget {
   final Completer onDone;
   const GroupsRetriever({super.key, required this.onDone});
@@ -46,10 +94,12 @@ class GroupsRetriever extends ConsumerWidget {
                   .where((e) => e.isNotEmpty)
                   .toSet();
 
-              ref.read(settingsProvider.notifier).replaceGroups(groups);
+              final filteredGroups = _filterGroups(groups);
+
+              ref.read(settingsProvider.notifier).replaceGroups(filteredGroups);
 
               onDone.complete();
-              talker.debug('Extracted & updated groups to: $groups');
+              talker.debug('Extracted & updated groups to: $filteredGroups');
             })
             .catchError((error) {
               talker.handle(error, null, 'Error extracting groups');
@@ -77,6 +127,7 @@ Future<void> showGroupsRetriever(BuildContext context) {
         Navigator.of(context).pop();
       })
       .catchError((error) {
+        talker.handle(  error, null, 'Error retrieving groups');
         Navigator.of(context).pop();
         throw error;
       });
