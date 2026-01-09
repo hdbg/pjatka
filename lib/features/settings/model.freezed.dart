b39a7a7f6cb3d65bd02834198f3bf938978bb712
hdbg <httpdebugger@protonmail.com>
2026-01-09 11:38:49 +0100

    ci: flutter codegen run

---
+++ lib/features/settings/model.freezed.dart
@@ -0,0 +1,289 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+// dart format off
+T _$identity<T>(T value) => value;
+
+/// @nodoc
+mixin _$SettingsState {
+
+ Set<String> get groups; int get minDateDaysOffset; int get maxDateDaysOffset;
+/// Create a copy of SettingsState
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+$SettingsStateCopyWith<SettingsState> get copyWith => _$SettingsStateCopyWithImpl<SettingsState>(this as SettingsState, _$identity);
+
+  /// Serializes this SettingsState to a JSON map.
+  Map<String, dynamic> toJson();
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is SettingsState&&const DeepCollectionEquality().equals(other.groups, groups)&&(identical(other.minDateDaysOffset, minDateDaysOffset) || other.minDateDaysOffset == minDateDaysOffset)&&(identical(other.maxDateDaysOffset, maxDateDaysOffset) || other.maxDateDaysOffset == maxDateDaysOffset));
+}
+
+@JsonKey(includeFromJson: false, includeToJson: false)
+@override
+int get hashCode => Object.hash(runtimeType,const DeepCollectionEquality().hash(groups),minDateDaysOffset,maxDateDaysOffset);
+
+@override
+String toString() {
+  return 'SettingsState(groups: $groups, minDateDaysOffset: $minDateDaysOffset, maxDateDaysOffset: $maxDateDaysOffset)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class $SettingsStateCopyWith<$Res>  {
+  factory $SettingsStateCopyWith(SettingsState value, $Res Function(SettingsState) _then) = _$SettingsStateCopyWithImpl;
+@useResult
+$Res call({
+ Set<String> groups, int minDateDaysOffset, int maxDateDaysOffset
+});
+
+
+
+
+}
+/// @nodoc
+class _$SettingsStateCopyWithImpl<$Res>
+    implements $SettingsStateCopyWith<$Res> {
+  _$SettingsStateCopyWithImpl(this._self, this._then);
+
+  final SettingsState _self;
+  final $Res Function(SettingsState) _then;
+
+/// Create a copy of SettingsState
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? groups = null,Object? minDateDaysOffset = null,Object? maxDateDaysOffset = null,}) {
+  return _then(_self.copyWith(
+groups: null == groups ? _self.groups : groups // ignore: cast_nullable_to_non_nullable
+as Set<String>,minDateDaysOffset: null == minDateDaysOffset ? _self.minDateDaysOffset : minDateDaysOffset // ignore: cast_nullable_to_non_nullable
+as int,maxDateDaysOffset: null == maxDateDaysOffset ? _self.maxDateDaysOffset : maxDateDaysOffset // ignore: cast_nullable_to_non_nullable
+as int,
+  ));
+}
+
+}
+
+
+/// Adds pattern-matching-related methods to [SettingsState].
+extension SettingsStatePatterns on SettingsState {
+/// A variant of `map` that fallback to returning `orElse`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeMap<TResult extends Object?>(TResult Function( _SettingsState value)?  $default,{required TResult orElse(),}){
+final _that = this;
+switch (_that) {
+case _SettingsState() when $default != null:
+return $default(_that);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// Callbacks receives the raw object, upcasted.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case final Subclass2 value:
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult map<TResult extends Object?>(TResult Function( _SettingsState value)  $default,){
+final _that = this;
+switch (_that) {
+case _SettingsState():
+return $default(_that);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `map` that fallback to returning `null`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? mapOrNull<TResult extends Object?>(TResult? Function( _SettingsState value)?  $default,){
+final _that = this;
+switch (_that) {
+case _SettingsState() when $default != null:
+return $default(_that);case _:
+  return null;
+
+}
+}
+/// A variant of `when` that fallback to an `orElse` callback.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeWhen<TResult extends Object?>(TResult Function( Set<String> groups,  int minDateDaysOffset,  int maxDateDaysOffset)?  $default,{required TResult orElse(),}) {final _that = this;
+switch (_that) {
+case _SettingsState() when $default != null:
+return $default(_that.groups,_that.minDateDaysOffset,_that.maxDateDaysOffset);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// As opposed to `map`, this offers destructuring.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case Subclass2(:final field2):
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult when<TResult extends Object?>(TResult Function( Set<String> groups,  int minDateDaysOffset,  int maxDateDaysOffset)  $default,) {final _that = this;
+switch (_that) {
+case _SettingsState():
+return $default(_that.groups,_that.minDateDaysOffset,_that.maxDateDaysOffset);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `when` that fallback to returning `null`
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? whenOrNull<TResult extends Object?>(TResult? Function( Set<String> groups,  int minDateDaysOffset,  int maxDateDaysOffset)?  $default,) {final _that = this;
+switch (_that) {
+case _SettingsState() when $default != null:
+return $default(_that.groups,_that.minDateDaysOffset,_that.maxDateDaysOffset);case _:
+  return null;
+
+}
+}
+
+}
+
+/// @nodoc
+@JsonSerializable()
+
+class _SettingsState implements SettingsState {
+  const _SettingsState({final  Set<String> groups = const {}, this.minDateDaysOffset = 7, this.maxDateDaysOffset = 30}): _groups = groups;
+  factory _SettingsState.fromJson(Map<String, dynamic> json) => _$SettingsStateFromJson(json);
+
+ final  Set<String> _groups;
+@override@JsonKey() Set<String> get groups {
+  if (_groups is EqualUnmodifiableSetView) return _groups;
+  // ignore: implicit_dynamic_type
+  return EqualUnmodifiableSetView(_groups);
+}
+
+@override@JsonKey() final  int minDateDaysOffset;
+@override@JsonKey() final  int maxDateDaysOffset;
+
+/// Create a copy of SettingsState
+/// with the given fields replaced by the non-null parameter values.
+@override @JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+_$SettingsStateCopyWith<_SettingsState> get copyWith => __$SettingsStateCopyWithImpl<_SettingsState>(this, _$identity);
+
+@override
+Map<String, dynamic> toJson() {
+  return _$SettingsStateToJson(this, );
+}
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _SettingsState&&const DeepCollectionEquality().equals(other._groups, _groups)&&(identical(other.minDateDaysOffset, minDateDaysOffset) || other.minDateDaysOffset == minDateDaysOffset)&&(identical(other.maxDateDaysOffset, maxDateDaysOffset) || other.maxDateDaysOffset == maxDateDaysOffset));
+}
+
+@JsonKey(includeFromJson: false, includeToJson: false)
+@override
+int get hashCode => Object.hash(runtimeType,const DeepCollectionEquality().hash(_groups),minDateDaysOffset,maxDateDaysOffset);
+
+@override
+String toString() {
+  return 'SettingsState(groups: $groups, minDateDaysOffset: $minDateDaysOffset, maxDateDaysOffset: $maxDateDaysOffset)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class _$SettingsStateCopyWith<$Res> implements $SettingsStateCopyWith<$Res> {
+  factory _$SettingsStateCopyWith(_SettingsState value, $Res Function(_SettingsState) _then) = __$SettingsStateCopyWithImpl;
+@override @useResult
+$Res call({
+ Set<String> groups, int minDateDaysOffset, int maxDateDaysOffset
+});
+
+
+
+
+}
+/// @nodoc
+class __$SettingsStateCopyWithImpl<$Res>
+    implements _$SettingsStateCopyWith<$Res> {
+  __$SettingsStateCopyWithImpl(this._self, this._then);
+
+  final _SettingsState _self;
+  final $Res Function(_SettingsState) _then;
+
+/// Create a copy of SettingsState
+/// with the given fields replaced by the non-null parameter values.
+@override @pragma('vm:prefer-inline') $Res call({Object? groups = null,Object? minDateDaysOffset = null,Object? maxDateDaysOffset = null,}) {
+  return _then(_SettingsState(
+groups: null == groups ? _self._groups : groups // ignore: cast_nullable_to_non_nullable
+as Set<String>,minDateDaysOffset: null == minDateDaysOffset ? _self.minDateDaysOffset : minDateDaysOffset // ignore: cast_nullable_to_non_nullable
+as int,maxDateDaysOffset: null == maxDateDaysOffset ? _self.maxDateDaysOffset : maxDateDaysOffset // ignore: cast_nullable_to_non_nullable
+as int,
+  ));
+}
+
+
+}
+
+// dart format on
---
+++ lib/features/settings/model.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_SettingsState _$SettingsStateFromJson(Map<String, dynamic> json) =>
+    _SettingsState(
+      groups:
+          (json['groups'] as List<dynamic>?)?.map((e) => e as String).toSet() ??
+          const {},
+      minDateDaysOffset: (json['minDateDaysOffset'] as num?)?.toInt() ?? 7,
+      maxDateDaysOffset: (json['maxDateDaysOffset'] as num?)?.toInt() ?? 30,
+    );
+
+Map<String, dynamic> _$SettingsStateToJson(_SettingsState instance) =>
+    <String, dynamic>{
+      'groups': instance.groups.toList(),
+      'minDateDaysOffset': instance.minDateDaysOffset,
+      'maxDateDaysOffset': instance.maxDateDaysOffset,
+    };
