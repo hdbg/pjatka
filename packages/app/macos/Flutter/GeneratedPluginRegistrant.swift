dc4cb64d4ff7d7055af70936c6cdfee677dfbbbe
hdbg <httpdebugger@protonmail.com>
2026-02-23 13:42:06 +0100

    housekeeping: update deps

--- packages/app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,6 @@ import Foundation
 
 import file_picker
 import flutter_inappwebview_macos
-import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
 import sqlite3_flutter_libs
@@ -16,7 +15,6 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
